153cd97bcfab089bf5f7ba9a7533dd46a0f97423
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-06-12 04:25:48 +0900

    iot-lambda pj first commit

---
+++ aws-iot-lambda-timestream/environments/dev/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.4.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = "ap-northeast-1"
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "terraform"
+
+  default_tags {
+    tags = {
+      Project = "iot-lambda-timestream"
+    }
+  }
+}
