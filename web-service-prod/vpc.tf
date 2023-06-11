ddce9a6cea8c9d620a3faf9439c83e85ad8eb8c9
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-06-12 04:07:43 +0900

    subnet作成

--- web-service-prod/vpc.tf
+++ web-service-prod/vpc.tf
@@ -10,3 +10,42 @@ resource "aws_vpc" "vpc" {
     Env     = "prod"
   }
 }
+
+########################################
+# サブネット の作成
+########################################
+resource "aws_subnet" "web-service-prod-alb-a-subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.100.0/24"
+
+  tags = {
+    Name    = "web-service-alb-a-subnet"
+    Project = "web-service"
+    Env     = "prod"
+  }
+}
+
+resource "aws_subnet" "web-service-prod-alb-c-subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.101.0/24"
+
+  tags = {
+    Name    = "web-service-alb-c-subnet"
+    Project = "web-service"
+    Env     = "prod"
+  }
+}
+
+resource "aws_subnet" "web-service-prod-ec2-a-subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.102.0/24"
+
+  tags = {
+    Name    = "web-service-ec2-a-subnet"
+    Project = "web-service"
+    Env     = "prod"
+  }
+}
